670b4c6a2a85a8d3b9db4b9c16da21704f588b42
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 23:45:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ holiday_hash.each do |season, holiday|
    name_array = name.to_s.split("_")
    name_strings_array = name_array.map{|string| string.capitalize}
    name_string = name_strings_array.join(" ")
+   name_strings_array
    puts name_strings_array + ": " + supplies.join(", ")
 end    
 end
