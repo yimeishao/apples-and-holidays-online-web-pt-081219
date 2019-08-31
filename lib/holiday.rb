36196378576bb9c72ed9a974851c39401ee6ff13
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 23:30:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,8 +48,6 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_hash[:winter].values.flatten
 end
-end
-end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
