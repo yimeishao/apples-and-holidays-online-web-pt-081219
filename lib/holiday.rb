08cea0d32a2084fc67a380b5df2d7b2a9d232800
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 23:34:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday| 
   
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
