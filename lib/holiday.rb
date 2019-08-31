f850be239ce19471b3e86b6aa687138dd723fb65
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 23:36:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday| 
   puts season.to_s.capitalize + ":"
-  season.each do |holiday, items|
+ holiday.each do |
     
 end
 end
