f1f862cd01ddee22e2f60ae1e1b800a708dc0a07
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 23:49:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.each do |season, holiday|
-  
+  holiday.each do |name, item| 
+    if item.include?("BBQ")
 end
 
 
