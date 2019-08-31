7aba41b33f7bd9a59ea962d45d5ef1927814ae27
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 23:24:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-holiday_hash[:winter][:my_birthday].push("gift")
+holiday_hash[:winter][:my_birthday].push(supply)
 holiday_hash
 end
 
