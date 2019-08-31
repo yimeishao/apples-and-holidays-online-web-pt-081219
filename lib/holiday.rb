6ee728137090438e9fa2a7a48711e869fb3a47f2
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 23:34:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |holiday, items| 
+holiday_hash.each do |season, holiday| 
   
 end
 
